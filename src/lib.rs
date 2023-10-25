c418f35d91792cb4d581a5e3d7edf468520fc5b0
LambdaXymox <lambda.xymox@gmail.com>
2023-10-25 17:14:53 -0500

    Eliminate why comparison feature.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
-mod abs_diff_eq;
-mod abs_diff_why_eq;
-mod relative_eq;
-mod relative_why_eq;
 mod tolerance;
+mod abs_diff_eq;
+mod relative_eq;
 
 
-pub use abs_diff_eq::*;
-pub use abs_diff_why_eq::*;
-pub use relative_eq::*;
-pub use relative_why_eq::*;
 pub use tolerance::*;
+pub use abs_diff_eq::*;
+pub use relative_eq::*;
 
