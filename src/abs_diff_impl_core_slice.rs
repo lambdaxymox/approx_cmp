c9dae8ae9e2038f6ff8898b04475816942753cbf
LambdaXymox <lambda.xymox@gmail.com>
2023-10-30 20:00:49 -0500

    Remove dead code.

--- src/abs_diff_impl_core_slice.rs
+++ src/abs_diff_impl_core_slice.rs
@@ -20,24 +20,8 @@ where
             .all(|((a, b), tol)| a.abs_diff_eq(b, tol))
     }
 }
-/*
-impl<'a, 'b, A, B> AbsDiffEq<&'b [B]> for &'a [A]
-where
-    A: AbsDiffEq<B>,
-    A::Tolerance: Sized
-{
-    type Tolerance = [A::Tolerance];
 
-    #[inline]
-    fn abs_diff_eq(&self, other: &&'b [B], max_abs_diff: &Self::Tolerance) -> bool {
-        self.len() == other.len() && 
-        self.iter()
-            .zip(other.iter())
-            .zip(max_abs_diff.iter())
-            .all(|((a, b), tol)| a.abs_diff_eq(b, tol))
-    }
-}
-*/
+
 impl<A, B> AbsDiffAllEq<[B]> for [A]
 where
     A: AbsDiffAllEq<B>
@@ -52,19 +36,4 @@ where
             .all(|(a, b)| a.abs_diff_all_eq(b, max_abs_diff))
     }
 }
-/*
-impl<'a, 'b, A, B> AbsDiffAllEq<&'b [B]> for &'a [A]
-where
-    A: AbsDiffAllEq<B>
-{
-    type AllTolerance = A::AllTolerance;
 
-    #[inline]
-    fn abs_diff_all_eq(&self, other: &&'b [B], max_abs_diff: &Self::AllTolerance) -> bool {
-        self.len() == other.len() && 
-        self.iter()
-            .zip(other.iter())
-            .all(|(a, b)| a.abs_diff_all_eq(b, max_abs_diff))
-    }
-}
-*/
