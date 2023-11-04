57630bc97bb10f128155fe9524cab32592edf6cc
LambdaXymox <lambda.xymox@gmail.com>
2023-11-03 23:54:46 -0500

    Remove dead code.

--- src/lib.rs
+++ src/lib.rs
@@ -13,47 +13,4 @@ extern crate approx_cmp_relative;
 
 pub use approx_cmp_abs_diff::*;
 pub use approx_cmp_relative::*;
-/*
-mod abs_diff;
-mod abs_diff_impl_core;
-mod abs_diff_impl_core_slice;
-mod abs_diff_impl_core_tuple;
-mod relative;
-mod relative_impl_core;
-mod relative_impl_core_slice;
-mod relative_impl_core_tuple;
 
-#[cfg(feature = "std")]
-mod abs_diff_impl_std_slice;
-
-#[cfg(feature = "std")]
-mod abs_diff_impl_std;
-
-#[cfg(feature = "std")]
-mod relative_impl_std_slice;
-
-#[cfg(feature = "std")]
-mod relative_impl_std;
-
-
-pub use abs_diff::*;
-pub use abs_diff_impl_core::*;
-pub use abs_diff_impl_core_slice::*;
-pub use abs_diff_impl_core_tuple::*;
-pub use relative::*;
-pub use relative_impl_core::*;
-pub use relative_impl_core_slice::*;
-pub use relative_impl_core_tuple::*;
-
-#[cfg(feature = "std")]
-pub use abs_diff_impl_std_slice::*;
-
-#[cfg(feature = "std")]
-pub use abs_diff_impl_std::*;
-
-#[cfg(feature = "std")]
-pub use relative_impl_std_slice::*;
-
-#[cfg(feature = "std")]
-pub use relative_impl_std::*;
-*/
