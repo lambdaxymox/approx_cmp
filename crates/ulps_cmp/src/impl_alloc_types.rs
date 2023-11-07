e721f0574555844941fa7bd15d1abe687ef4fbe4
LambdaXymox <lambda.xymox@gmail.com>
2023-11-07 17:24:18 -0600

    Add support for Arc.

--- crates/ulps_cmp/src/impl_alloc_types.rs
+++ crates/ulps_cmp/src/impl_alloc_types.rs
@@ -6,6 +6,7 @@ use crate::traits::{
 };
 use std::boxed::Box;
 use std::rc::Rc;
+use std::sync::Arc;
 use std::fmt;
 
 
@@ -37,6 +38,20 @@ where
     }
 }
 
+impl<A, B> UlpsEq<Arc<B>> for Arc<A>
+where
+    A: UlpsEq<B> + ?Sized,
+    B: ?Sized,
+{
+    type Tolerance = A::Tolerance;
+    type UlpsTolerance = A::UlpsTolerance;
+
+    #[inline]
+    fn ulps_eq(&self, other: &Arc<B>, max_abs_diff: &Self::Tolerance, max_ulps: &Self::UlpsTolerance) -> bool {
+        UlpsEq::ulps_eq(&**self, &**other, max_abs_diff, max_ulps)
+    }
+}
+
 
 impl<A, B> UlpsAllEq<Box<B>> for Box<A>
 where
@@ -62,6 +77,20 @@ where
 
     #[inline]
     fn ulps_all_eq(&self, other: &Rc<B>, max_abs_diff: &Self::AllTolerance, max_ulps: &Self::AllUlpsTolerance) -> bool {
+        UlpsAllEq::ulps_all_eq(&**self, &**other, max_abs_diff, max_ulps)
+    }
+}
+
+impl<A, B> UlpsAllEq<Arc<B>> for Arc<A>
+where
+    A: UlpsAllEq<B> + ?Sized,
+    B: ?Sized,
+{
+    type AllTolerance = A::AllTolerance;
+    type AllUlpsTolerance = A::AllUlpsTolerance;
+
+    #[inline]
+    fn ulps_all_eq(&self, other: &Arc<B>, max_abs_diff: &Self::AllTolerance, max_ulps: &Self::AllUlpsTolerance) -> bool {
         UlpsAllEq::ulps_all_eq(&**self, &**other, max_abs_diff, max_ulps)
     }
 }
@@ -129,6 +158,37 @@ where
     }
 }
 
+impl<A, B> AssertUlpsEq<Arc<B>> for Arc<A>
+where
+    A: AssertUlpsEq<B> + ?Sized + Copy,
+    B: ?Sized + Copy,
+{
+    type DebugAbsDiff = A::DebugAbsDiff;
+    type DebugUlpsDiff = A::DebugUlpsDiff;
+    type DebugTolerance = A::DebugTolerance;
+    type DebugUlpsTolerance = A::DebugUlpsTolerance;
+
+    #[inline]
+    fn debug_abs_diff(&self, other: &Arc<B>) -> Self::DebugAbsDiff {
+        AssertUlpsEq::debug_abs_diff(&**self, &**other)
+    }
+
+    #[inline]
+    fn debug_ulps_diff(&self, other: &Arc<B>) -> Self::DebugUlpsDiff {
+        AssertUlpsEq::debug_ulps_diff(&**self, &**other)
+    }
+
+    #[inline]
+    fn debug_abs_diff_tolerance(&self, other: &Arc<B>, max_abs_diff: &Self::Tolerance) -> Self::DebugTolerance {
+        AssertUlpsEq::debug_abs_diff_tolerance(&**self, &**other, max_abs_diff)
+    }
+
+    #[inline]
+    fn debug_ulps_tolerance(&self, other: &Arc<B>, max_ulps: &Self::UlpsTolerance) -> Self::DebugUlpsTolerance {
+        AssertUlpsEq::debug_ulps_tolerance(&**self, &**other, max_ulps)
+    }
+}
+
 
 impl<A, B> AssertUlpsAllEq<Box<B>> for Box<A>
 where
@@ -164,6 +224,25 @@ where
 
     #[inline]
     fn debug_ulps_all_tolerance(&self, other: &Rc<B>, max_ulps: &Self::AllUlpsTolerance) -> Self::AllDebugUlpsTolerance {
+        AssertUlpsAllEq::debug_ulps_all_tolerance(&**self, &**other, max_ulps)
+    }
+}
+
+impl<A, B> AssertUlpsAllEq<Arc<B>> for Arc<A>
+where
+    A: AssertUlpsAllEq<B> + ?Sized + Copy,
+    B: ?Sized + Copy,
+{
+    type AllDebugTolerance = A::AllDebugTolerance;
+    type AllDebugUlpsTolerance = A::AllDebugUlpsTolerance;
+
+    #[inline]
+    fn debug_abs_diff_all_tolerance(&self, other: &Arc<B>, max_abs_diff: &Self::AllTolerance) -> Self::AllDebugTolerance {
+        AssertUlpsAllEq::debug_abs_diff_all_tolerance(&**self, &**other, max_abs_diff)
+    }
+
+    #[inline]
+    fn debug_ulps_all_tolerance(&self, other: &Arc<B>, max_ulps: &Self::AllUlpsTolerance) -> Self::AllDebugUlpsTolerance {
         AssertUlpsAllEq::debug_ulps_all_tolerance(&**self, &**other, max_ulps)
     }
 }
