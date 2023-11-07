07c18c6d45449d846e55dcc3dec861f049fb64b8
LambdaXymox <lambda.xymox@gmail.com>
2023-11-07 16:34:20 -0600

    Remove dead code.

--- crates/relative_cmp/src/impl_alloc_slice.rs
+++ crates/relative_cmp/src/impl_alloc_slice.rs
@@ -53,56 +53,7 @@ where
         }
     }
 }
-/*
-impl<'a, 'b, A, B> AssertRelativeEq<&'b [B]> for &'a [A]
-where
-    A: AssertRelativeEq<B>,
-    A::Tolerance: Sized,
-    A::DebugTolerance: Sized,
-{
-    type DebugAbsDiff = Option<Vec<A::DebugAbsDiff>>;
-    type DebugTolerance = Option<Vec<A::DebugTolerance>>;
 
-    #[inline]
-    fn debug_abs_diff(&self, other: &&'b [B]) -> Self::DebugAbsDiff {
-        if self.len() == other.len() {
-            Some(self.iter().zip(other.iter()).map(|(a, b)| a.debug_abs_diff(b)).collect())
-        } else {
-            None
-        }
-    }
-
-    #[inline]
-    fn debug_abs_diff_tolerance(&self, other: &&'b [B], max_abs_diff: &Self::Tolerance) -> Self::DebugTolerance {
-        if (self.len() == other.len()) && (self.len() == max_abs_diff.len()) {
-            Some(
-                self.iter()
-                    .zip(other.iter())
-                    .zip(max_abs_diff.iter())
-                    .map(|((a, b), tol)| AssertRelativeEq::debug_abs_diff_tolerance(a, b, tol))
-                    .collect(),
-            )
-        } else {
-            None
-        }
-    }
-
-    #[inline]
-    fn debug_relative_tolerance(&self, other: &&'b [B], max_relative: &Self::Tolerance) -> Self::DebugTolerance {
-        if (self.len() == other.len()) && (self.len() == max_relative.len()) {
-            Some(
-                self.iter()
-                    .zip(other.iter())
-                    .zip(max_relative.iter())
-                    .map(|((a, b), tol)| AssertRelativeEq::debug_relative_tolerance(a, b, tol))
-                    .collect(),
-            )
-        } else {
-            None
-        }
-    }
-}
-*/
 impl<A, B> AssertRelativeAllEq<[B]> for [A]
 where
     A: AssertRelativeAllEq<B>,
@@ -139,41 +90,3 @@ where
         }
     }
 }
-/*
-impl<'a, 'b, A, B> AssertRelativeAllEq<&'b [B]> for &'a [A]
-where
-    A: AssertRelativeAllEq<B>,
-    A::AllTolerance: Sized,
-    A::AllDebugTolerance: Sized,
-{
-    type AllDebugTolerance = Option<Vec<A::AllDebugTolerance>>;
-
-    #[inline]
-    fn debug_abs_diff_all_tolerance(&self, other: &&'b [B], max_abs_diff: &Self::AllTolerance) -> Self::AllDebugTolerance {
-        if self.len() == other.len() {
-            Some(
-                self.iter()
-                    .zip(other.iter())
-                    .map(|(a, b)| AssertRelativeAllEq::debug_abs_diff_all_tolerance(a, b, max_abs_diff))
-                    .collect(),
-            )
-        } else {
-            None
-        }
-    }
-
-    #[inline]
-    fn debug_relative_all_tolerance(&self, other: &&'b [B], max_relative: &Self::AllTolerance) -> Self::AllDebugTolerance {
-        if self.len() == other.len() {
-            Some(
-                self.iter()
-                    .zip(other.iter())
-                    .map(|(a, b)| AssertRelativeAllEq::debug_relative_all_tolerance(a, b, max_relative))
-                    .collect(),
-            )
-        } else {
-            None
-        }
-    }
-}
-*/
--- crates/relative_cmp/src/impl_core_slice.rs
+++ crates/relative_cmp/src/impl_core_slice.rs
@@ -24,28 +24,7 @@ where
                 .all(|(((a, b), abs_tol), rel_tol)| a.relative_eq(b, abs_tol, rel_tol))
     }
 }
-/*
-impl<'a, 'b, A, B> RelativeEq<&'b [B]> for &'a [A]
-where
-    A: RelativeEq<B>,
-    A::Tolerance: Sized,
-{
-    type Tolerance = [A::Tolerance];
 
-    #[inline]
-    fn relative_eq(&self, other: &&'b [B], max_abs_diff: &Self::Tolerance, max_relative: &Self::Tolerance) -> bool {
-        self.len() == other.len()
-            && self.len() == max_abs_diff.len()
-            && self.len() == max_relative.len()
-            && self
-                .iter()
-                .zip(other.iter())
-                .zip(max_abs_diff.iter())
-                .zip(max_relative.iter())
-                .all(|(((a, b), abs_tol), rel_tol)| a.relative_eq(b, abs_tol, rel_tol))
-    }
-}
-*/
 impl<A, B> RelativeAllEq<[B]> for [A]
 where
     A: RelativeAllEq<B>,
@@ -61,20 +40,3 @@ where
                 .all(|(a, b)| a.relative_all_eq(b, max_abs_diff, max_relative))
     }
 }
-/*
-impl<'a, 'b, A, B> RelativeAllEq<&'b [B]> for &'a [A]
-where
-    A: RelativeAllEq<B>,
-{
-    type AllTolerance = A::AllTolerance;
-
-    #[inline]
-    fn relative_all_eq(&self, other: &&'b [B], max_abs_diff: &Self::AllTolerance, max_relative: &Self::AllTolerance) -> bool {
-        self.len() == other.len()
-            && self
-                .iter()
-                .zip(other.iter())
-                .all(|(a, b)| a.relative_all_eq(b, max_abs_diff, max_relative))
-    }
-}
-*/
