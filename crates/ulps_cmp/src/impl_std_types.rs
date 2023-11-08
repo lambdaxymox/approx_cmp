ade976fb89243c03f848b1c8a0a4965ae183f764
LambdaXymox <lambda.xymox@gmail.com>
2023-11-08 09:33:31 -0600

    Add support for OnceLock.

--- crates/ulps_cmp/src/impl_std_types.rs
+++ crates/ulps_cmp/src/impl_std_types.rs
@@ -7,6 +7,7 @@ use crate::traits::{
 use std::fmt;
 use std::hash;
 use std::collections::HashMap;
+use std::sync;
 
 
 impl<K, VA, VB, S> UlpsEq<HashMap<K, VB, S>> for HashMap<K, VA, S>
@@ -39,6 +40,26 @@ where
     }
 }
 
+impl<A, B> UlpsEq<sync::OnceLock<B>> for sync::OnceLock<A>
+where
+    A: UlpsEq<B>,
+    A::Tolerance: Sized,
+    A::UlpsTolerance: Sized,
+{
+    type Tolerance = A::Tolerance;
+    type UlpsTolerance = A::UlpsTolerance;
+
+    #[inline]
+    fn ulps_eq(&self, other: &sync::OnceLock<B>, max_abs_diff: &Self::Tolerance, max_ulps: &Self::UlpsTolerance) -> bool {
+        if let (Some(a), Some(b)) = (self.get(), other.get()) {
+            UlpsEq::ulps_eq(a, b, max_abs_diff, max_ulps)
+        } else {
+            false
+        }
+    }
+}
+
+
 impl<K, VA, VB, S> UlpsAllEq<HashMap<K, VB, S>> for HashMap<K, VA, S>
 where
     K: Eq + hash::Hash,
@@ -60,6 +81,26 @@ where
             })
     }
 }
+
+impl<A, B> UlpsAllEq<sync::OnceLock<B>> for sync::OnceLock<A>
+where
+    A: UlpsAllEq<B>,
+    A::AllTolerance: Sized,
+    A::AllUlpsTolerance: Sized,
+{
+    type AllTolerance = A::AllTolerance;
+    type AllUlpsTolerance = A::AllUlpsTolerance;
+
+    #[inline]
+    fn ulps_all_eq(&self, other: &sync::OnceLock<B>, max_abs_diff: &Self::AllTolerance, max_ulps: &Self::AllUlpsTolerance) -> bool {
+        if let (Some(a), Some(b)) = (self.get(), other.get()) {
+            UlpsAllEq::ulps_all_eq(a, b, max_abs_diff, max_ulps)
+        } else {
+            false
+        }
+    }
+}
+
 
 impl<K, VA, VB, S> AssertUlpsEq<HashMap<K, VB, S>> for HashMap<K, VA, S>
 where
@@ -129,6 +170,55 @@ where
     }
 }
 
+impl<A, B> AssertUlpsEq<sync::OnceLock<B>> for sync::OnceLock<A>
+where
+    A: AssertUlpsEq<B>,
+    A::Tolerance: Sized,
+    A::UlpsTolerance: Sized,
+{
+    type DebugAbsDiff = Option<A::DebugAbsDiff>;
+    type DebugUlpsDiff = Option<A::DebugUlpsDiff>;
+    type DebugTolerance = Option<A::DebugTolerance>;
+    type DebugUlpsTolerance = Option<A::DebugUlpsTolerance>;
+
+    #[inline]
+    fn debug_abs_diff(&self, other: &sync::OnceLock<B>) -> Self::DebugAbsDiff {
+        if let (Some(a), Some(b)) = (self.get(), other.get()) {
+            Some(AssertUlpsEq::debug_abs_diff(a, b))
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    fn debug_ulps_diff(&self, other: &sync::OnceLock<B>) -> Self::DebugUlpsDiff {
+        if let (Some(a), Some(b)) = (self.get(), other.get()) {
+            Some(AssertUlpsEq::debug_ulps_diff(a, b))
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    fn debug_abs_diff_tolerance(&self, other: &sync::OnceLock<B>, max_abs_diff: &Self::Tolerance) -> Self::DebugTolerance {
+        if let (Some(a), Some(b)) = (self.get(), other.get()) {
+            Some(AssertUlpsEq::debug_abs_diff_tolerance(a, b, max_abs_diff))
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    fn debug_ulps_tolerance(&self, other: &sync::OnceLock<B>, max_ulps: &Self::UlpsTolerance) -> Self::DebugUlpsTolerance {
+        if let (Some(a), Some(b)) = (self.get(), other.get()) {
+            Some(AssertUlpsEq::debug_ulps_tolerance(a, b, max_ulps))
+        } else {
+            None
+        }
+    }
+}
+
+
 impl<K, VA, VB, S> AssertUlpsAllEq<HashMap<K, VB, S>> for HashMap<K, VA, S>
 where
     K: Eq + hash::Hash + Clone + fmt::Debug,
@@ -161,6 +251,34 @@ where
                 result.insert(key.clone(), v.debug_ulps_all_tolerance(other.get(key)?, max_ulps));
             }
             Some(result)
+        } else {
+            None
+        }
+    }
+}
+
+impl<A, B> AssertUlpsAllEq<sync::OnceLock<B>> for sync::OnceLock<A>
+where
+    A: AssertUlpsAllEq<B>,
+    A::AllTolerance: Sized,
+    A::AllUlpsTolerance: Sized,
+{
+    type AllDebugTolerance = Option<A::AllDebugTolerance>;
+    type AllDebugUlpsTolerance = Option<A::AllDebugUlpsTolerance>;
+
+    #[inline]
+    fn debug_abs_diff_all_tolerance(&self, other: &sync::OnceLock<B>, max_abs_diff: &Self::AllTolerance) -> Self::AllDebugTolerance {
+        if let (Some(a), Some(b)) = (self.get(), other.get()) {
+            Some(AssertUlpsAllEq::debug_abs_diff_all_tolerance(a, b, max_abs_diff))
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    fn debug_ulps_all_tolerance(&self, other: &sync::OnceLock<B>, max_ulps: &Self::AllUlpsTolerance) -> Self::AllDebugUlpsTolerance {
+        if let (Some(a), Some(b)) = (self.get(), other.get()) {
+            Some(AssertUlpsAllEq::debug_ulps_all_tolerance(a, b, max_ulps))
         } else {
             None
         }
---
+++ crates/ulps_cmp/tests/test_once_lock.rs
@@ -0,0 +1,379 @@
+extern crate ulps_cmp;
+
+
+use std::sync;
+use ulps_cmp::{
+    assert_ulps_eq,
+    assert_ulps_ne,
+    AssertUlpsAllEq,
+    AssertUlpsEq,
+};
+
+
+#[rustfmt::skip]
+#[test]
+fn test_eq() {
+    let lhs = sync::OnceLock::from([
+        0.9999999_f32, 2.0000000_f32, 2.9999995_f32, 4.0000000_f32,
+        4.9999999_f32, 6.0000000_f32, 6.9999995_f32, 8.0000000_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.0000000_f32, 1.9999995_f32, 3.0000000_f32, 4.0000005_f32,
+        5.0000000_f32, 6.0000001_f32, 7.0000000_f32, 7.9999995_f32,
+    ]);
+    let eps = f32::EPSILON;
+    let max_abs_diff = [
+        1.0_f32 * eps, 4.0_f32 * eps, 4.0_f32 * eps, 4.0_f32 * eps,
+        1.0_f32 * eps, 1.0_f32 * eps, 4.0_f32 * eps, 4.0_f32 * eps,
+    ];
+    let max_ulps = [
+        2_u32, 4_u32, 2_u32, 1_u32, 
+        1_u32, 1_u32, 1_u32, 1_u32,
+    ];
+
+    assert_ulps_eq!(lhs, rhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+    assert_ulps_eq!(rhs, lhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_ne1() {
+    let lhs = sync::OnceLock::from([
+        0.9999999_f32, 2.0000000_f32, 2.9999995_f32, 4.0000000_f32,
+        4.9999999_f32, 6.0000000_f32, 6.9999995_f32, 8.0000000_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.0000000_f32, 1.9999995_f32, 3.0000000_f32, 4.0000005_f32,
+        5.0000000_f32, 6.0000001_f32, 7.0000000_f32, 7.9999995_f32,
+    ]);
+    let eps = f32::EPSILON;
+    let max_abs_diff = [
+        0.5_f32 * eps, 4.0_f32 * eps, 4.0_f32 * eps, 4.0_f32 * eps,
+        0.5_f32 * eps, 0.5_f32 * eps, 4.0_f32 * eps, 4.0_f32 * eps,
+    ];
+    let max_ulps = [
+        1_u32, 3_u32, 1_u32, 1_u32,
+        1_u32, 1_u32, 1_u32, 1_u32,
+    ];
+
+    assert_ulps_ne!(lhs, rhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+    assert_ulps_ne!(rhs, lhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_ne2() {
+    let lhs = sync::OnceLock::from([
+        0.9999999_f32, 2.0000000_f32, 2.9999995_f32, 4.0000000_f32,
+        4.9999999_f32, 6.0000000_f32, 6.9999995_f32, 8.0000000_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.0000000_f32, 1.9999995_f32, 3.0000000_f32, 4.0000005_f32,
+        5.0000000_f32, 6.0000001_f32, 7.0000000_f32, 7.9999995_f32,
+    ]);
+    let eps = f32::EPSILON;
+    let max_abs_diff = [
+        1.0_f32 * eps, 2.0_f32 * eps, 2.0_f32 * eps, 2.0_f32 * eps,
+        1.0_f32 * eps, 1.0_f32 * eps, 2.0_f32 * eps, 2.0_f32 * eps,
+    ];
+    let max_ulps = [1_u32; 8];
+
+    assert_ulps_ne!(lhs, rhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+    assert_ulps_ne!(rhs, lhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_all_eq() {
+    let lhs = sync::OnceLock::from([
+        0.9999999_f32, 2.0000000_f32, 2.9999995_f32, 4.0000000_f32,
+        4.9999999_f32, 6.0000000_f32, 6.9999995_f32, 8.0000000_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.0000000_f32, 1.9999995_f32, 3.0000000_f32, 4.0000005_f32,
+        5.0000000_f32, 6.0000001_f32, 7.0000000_f32, 7.9999995_f32,
+    ]);
+    let max_abs_diff = 4.0_f32 * f32::EPSILON;
+    let max_ulps = 4_u32;
+
+    assert_ulps_eq!(lhs, rhs, abs_diff_all <= max_abs_diff, ulps_all <= max_ulps);
+    assert_ulps_eq!(rhs, lhs, abs_diff_all <= max_abs_diff, ulps_all <= max_ulps);
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_all_ne() {
+    let lhs = sync::OnceLock::from([
+        0.9999999_f32, 2.0000000_f32, 2.9999995_f32, 4.0000000_f32,
+        4.9999999_f32, 6.0000000_f32, 6.9999995_f32, 8.0000000_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.0000000_f32, 1.9999995_f32, 3.0000000_f32, 4.0000005_f32,
+        5.0000000_f32, 6.0000001_f32, 7.0000000_f32, 7.9999995_f32,
+    ]);
+    let max_abs_diff = 2.0_f32 * f32::EPSILON;
+    let max_ulps = 1_u32;
+
+    assert_ulps_ne!(lhs, rhs, abs_diff_all <= max_abs_diff, ulps_all <= max_ulps);
+    assert_ulps_ne!(rhs, lhs, abs_diff_all <= max_abs_diff, ulps_all <= max_ulps);
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_debug_abs_diff1() {
+    let lhs = sync::OnceLock::from([
+        1.00_f32, 1.25_f32, 1.50_f32, 2.00_f32,
+        2.50_f32, 3.00_f32, 4.00_f32, 5.00_f32,
+    ]);
+    let abs_diff = [0.0000000_f32; 8];
+
+    assert_eq!(lhs.debug_abs_diff(&lhs), Some(abs_diff));
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_debug_abs_diff2() {
+    let lhs = sync::OnceLock::from([
+        1.00_f32, 1.25_f32, 1.50_f32, 2.00_f32,
+        2.50_f32, 3.00_f32, 4.00_f32, 5.00_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.10_f32, 1.15_f32, 1.70_f32, 1.80_f32,
+        2.80_f32, 2.70_f32, 4.40_f32, 4.60_f32,
+    ]);
+    let abs_diff = [
+        0.100000024_f32, 0.100000024_f32, 0.20000005_f32, 0.20000005_f32,
+        0.299999950_f32, 0.299999950_f32, 0.40000010_f32, 0.40000010_f32,
+    ];
+
+    assert_eq!(lhs.debug_abs_diff(&rhs), Some(abs_diff));
+    assert_eq!(rhs.debug_abs_diff(&lhs), Some(abs_diff));
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_debug_abs_diff3() {
+    let lhs = sync::OnceLock::from([
+        0.9999500_f32, 2.0000000_f32, 2.9999500_f32, 4.0000000_f32,
+        4.9999500_f32, 6.0000000_f32, 6.9999500_f32, 8.0000000_f32,
+    ]);
+    let abs_diff = [0.00000000000000_f32; 8];
+
+    assert_eq!(lhs.debug_abs_diff(&lhs), Some(abs_diff));
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_debug_abs_diff4() {
+    let lhs = sync::OnceLock::from([
+        0.9999500_f32, 2.0000000_f32, 2.9999500_f32, 4.0000000_f32,
+        4.9999500_f32, 6.0000000_f32, 6.9999500_f32, 8.0000000_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.0000000_f32, 1.9999500_f32, 3.0000000_f32, 4.0000005_f32,
+        5.0000000_f32, 6.0000000_f32, 7.0000000_f32, 8.0000000_f32,
+    ]);
+    let abs_diff = [
+        0.00005000829700_f32, 0.00004994869200_f32, 0.00005006790000_f32, 0.00000047683716_f32,
+        0.00005006790000_f32, 0.00000000000000_f32, 0.00005006790000_f32, 0.00000000000000_f32,
+    ];
+
+    assert_eq!(lhs.debug_abs_diff(&rhs), Some(abs_diff));
+    assert_eq!(rhs.debug_abs_diff(&lhs), Some(abs_diff));
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_debug_ulps_diff1() {
+    let lhs = sync::OnceLock::from([
+        1.00_f32, 1.25_f32, 1.50_f32, 2.00_f32,
+        2.50_f32, 3.00_f32, 4.00_f32, 5.00_f32,
+    ]);
+    let ulps_diff = [Some(0_u32); 8];
+
+    assert_eq!(lhs.debug_ulps_diff(&lhs), Some(ulps_diff));
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_debug_ulps_diff2() {
+    let lhs = sync::OnceLock::from([
+        1.00_f32, 1.25_f32, 1.50_f32, 2.00_f32,
+        2.50_f32, 3.00_f32, 4.00_f32, 5.00_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.10_f32, 1.15_f32, 1.70_f32, 1.80_f32,
+        2.80_f32, 2.70_f32, 4.40_f32, 4.60_f32,
+    ]);
+    let ulps_diff = [
+        Some(838861_u32),  Some(838861_u32),  Some(1677722_u32), Some(1677722_u32),
+        Some(1258291_u32), Some(1258291_u32), Some(838861_u32),  Some(838861_u32),
+    ];
+
+    assert_eq!(lhs.debug_ulps_diff(&rhs), Some(ulps_diff));
+    assert_eq!(rhs.debug_ulps_diff(&lhs), Some(ulps_diff));
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_debug_ulps_diff3() {
+    let lhs = sync::OnceLock::from([
+        0.9999500_f32, 2.0000000_f32, 2.9999500_f32, 4.0000000_f32,
+        4.9999500_f32, 6.0000000_f32, 6.9999500_f32, 8.0000000_f32,
+    ]);
+    let ulps_diff = [Some(0_u32); 8];
+
+    assert_eq!(lhs.debug_ulps_diff(&lhs), Some(ulps_diff));
+}
+
+#[rustfmt::skip]
+#[test]
+fn test_debug_ulps_diff4() {
+    let lhs = sync::OnceLock::from([
+        0.9999500_f32, 2.0000000_f32, 2.9999500_f32, 4.0000000_f32,
+        4.9999500_f32, 6.0000000_f32, 6.9999500_f32, 8.0000000_f32,
+    ]);
+    let rhs = sync::OnceLock::from([
+        1.0000000_f32, 1.9999500_f32, 3.0000000_f32, 4.0000005_f32,
+        5.0000000_f32, 6.0000000_f32, 7.0000000_f32, 8.0000000_f32,
+    ]);
+    let ulps_diff = [
+        Some(839_u32), Some(419_u32), Some(210_u32), Some(1_u32),
+        Some(105_u32), Some(0_u32),   Some(105_u32), Some(0_u32),
+    ];
+
+    assert_eq!(lhs.debug_ulps_diff(&rhs), Some(ulps_diff));
+    assert_eq!(rhs.debug_ulps_diff(&lhs), Some(ulps_diff));
+}
+
+#[test]
+fn test_debug_abs_diff_tolerance() {
+    let lhs = sync::OnceLock::from([2.00_f32, 3.25_f32, 4.50_f32, 5.75_f32]);
+    let rhs = sync::OnceLock::from([2.50_f32, 3.00_f32, 4.00_f32, 6.00_f32]);
+    let max_abs_diff = [0.10_f32, 0.20_f32, 0.30_f32, 0.40_f32];
+
+    assert_eq!(lhs.debug_abs_diff_tolerance(&rhs, &max_abs_diff), Some(max_abs_diff));
+    assert_eq!(rhs.debug_abs_diff_tolerance(&lhs, &max_abs_diff), Some(max_abs_diff));
+}
+
+#[test]
+fn test_debug_abs_diff_all_tolerance() {
+    let lhs = sync::OnceLock::from([2.00_f32, 3.25_f32, 4.50_f32, 5.75_f32]);
+    let rhs = sync::OnceLock::from([2.50_f32, 3.00_f32, 4.00_f32, 6.00_f32]);
+    let max_abs_diff_all = 0.20_f32;
+    let max_abs_diff = [max_abs_diff_all; 4];
+
+    assert_eq!(lhs.debug_abs_diff_all_tolerance(&rhs, &max_abs_diff_all), Some(max_abs_diff));
+    assert_eq!(rhs.debug_abs_diff_all_tolerance(&lhs, &max_abs_diff_all), Some(max_abs_diff));
+}
+
+#[test]
+fn test_debug_ulps_tolerance() {
+    let lhs = sync::OnceLock::from([2.00_f32, 3.25_f32, 4.50_f32, 5.75_f32]);
+    let rhs = sync::OnceLock::from([2.50_f32, 3.00_f32, 4.00_f32, 6.00_f32]);
+    let max_ulps = [2_u32, 4_u32, 8_u32, 16_u32];
+
+    assert_eq!(lhs.debug_ulps_tolerance(&rhs, &max_ulps), Some(max_ulps));
+    assert_eq!(rhs.debug_ulps_tolerance(&lhs, &max_ulps), Some(max_ulps));
+}
+
+#[test]
+fn test_debug_ulps_all_tolerance() {
+    let lhs = sync::OnceLock::from([2.00_f32, 3.25_f32, 4.50_f32, 5.75_f32]);
+    let rhs = sync::OnceLock::from([2.50_f32, 3.00_f32, 4.00_f32, 6.00_f32]);
+    let max_ulps_all = 8_u32;
+    let max_ulps = [max_ulps_all; 4];
+
+    assert_eq!(lhs.debug_ulps_all_tolerance(&rhs, &max_ulps_all), Some(max_ulps));
+    assert_eq!(rhs.debug_ulps_all_tolerance(&lhs, &max_ulps_all), Some(max_ulps));
+}
+
+#[test]
+fn test_eq_none() {
+    let lhs: sync::OnceLock<f32> = sync::OnceLock::from(1.0_f32);
+    let rhs: sync::OnceLock<f32> = sync::OnceLock::from(1.0_f32);
+    let max_abs_diff = f32::EPSILON;
+    let max_ulps = 1_u32;
+
+    assert_ulps_eq!(lhs, rhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+}
+
+#[test]
+fn test_ne_none1() {
+    let lhs: sync::OnceLock<f32> = sync::OnceLock::new();
+    let rhs: sync::OnceLock<f32> = sync::OnceLock::new();
+    let max_abs_diff = f32::EPSILON;
+    let max_ulps = 8_u32;
+
+    assert_ulps_ne!(lhs, rhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+}
+
+#[test]
+fn test_ne_none2() {
+    let lhs: sync::OnceLock<f32> = sync::OnceLock::new();
+    let rhs: sync::OnceLock<f32> = sync::OnceLock::from(1.0_f32);
+    let max_abs_diff = f32::EPSILON;
+    let max_ulps = 8_u32;
+
+    assert_ulps_ne!(lhs, rhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+}
+
+#[test]
+fn test_ne_none3() {
+    let lhs: sync::OnceLock<f32> = sync::OnceLock::from(1.0_f32);
+    let rhs: sync::OnceLock<f32> = sync::OnceLock::new();
+    let max_abs_diff = f32::EPSILON;
+    let max_ulps = 8_u32;
+
+    assert_ulps_ne!(lhs, rhs, abs_diff <= max_abs_diff, ulps <= max_ulps);
+}
+
+#[test]
+fn test_debug_abs_diff_tolerance_none() {
+    let lhs = sync::OnceLock::from([2.00_f32, 3.25_f32, 4.50_f32, 5.75_f32]);
+    let rhs = sync::OnceLock::from([2.50_f32, 3.00_f32, 4.00_f32, 6.00_f32]);
+    let empty_cell = sync::OnceLock::new();
+    let max_abs_diff = [0.10_f32, 0.20_f32, 0.30_f32, 0.40_f32];
+
+    assert_eq!(lhs.debug_abs_diff_tolerance(&rhs, &max_abs_diff), Some(max_abs_diff));
+    assert_eq!(lhs.debug_abs_diff_tolerance(&empty_cell, &max_abs_diff), None);
+    assert_eq!(empty_cell.debug_abs_diff_tolerance(&rhs, &max_abs_diff), None);
+}
+
+#[test]
+fn test_debug_abs_diff_all_tolerance_none() {
+    let lhs = sync::OnceLock::from([2.00_f32, 3.25_f32, 4.50_f32, 5.75_f32]);
+    let rhs = sync::OnceLock::from([2.50_f32, 3.00_f32, 4.00_f32, 6.00_f32]);
+    let empty_cell = sync::OnceLock::new();
+    let max_abs_diff_all = 0.20_f32;
+    let max_abs_diff = [max_abs_diff_all; 4];
+
+    assert_eq!(lhs.debug_abs_diff_all_tolerance(&rhs, &max_abs_diff_all), Some(max_abs_diff));
+    assert_eq!(lhs.debug_abs_diff_all_tolerance(&empty_cell, &max_abs_diff_all), None);
+    assert_eq!(empty_cell.debug_abs_diff_all_tolerance(&rhs, &max_abs_diff_all), None);
+}
+
+#[test]
+fn test_debug_ulps_tolerance_none() {
+    let lhs = sync::OnceLock::from([2.00_f32, 3.25_f32, 4.50_f32, 5.75_f32]);
+    let rhs = sync::OnceLock::from([2.50_f32, 3.00_f32, 4.00_f32, 6.00_f32]);
+    let empty_cell = sync::OnceLock::new();
+    let max_ulps = [2_u32, 4_u32, 8_u32, 16_u32];
+
+    assert_eq!(lhs.debug_ulps_tolerance(&rhs, &max_ulps), Some(max_ulps));
+    assert_eq!(lhs.debug_ulps_tolerance(&empty_cell, &max_ulps), None);
+    assert_eq!(empty_cell.debug_ulps_tolerance(&rhs, &max_ulps), None);
+}
+
+#[test]
+fn test_debug_ulps_all_tolerance_none() {
+    let lhs = sync::OnceLock::from([2.00_f32, 3.25_f32, 4.50_f32, 5.75_f32]);
+    let rhs = sync::OnceLock::from([2.50_f32, 3.00_f32, 4.00_f32, 6.00_f32]);
+    let empty_cell = sync::OnceLock::new();
+    let max_ulps_all = 4_u32;
+    let max_ulps = [max_ulps_all; 4];
+
+    assert_eq!(lhs.debug_ulps_all_tolerance(&rhs, &max_ulps_all), Some(max_ulps));
+    assert_eq!(lhs.debug_ulps_all_tolerance(&empty_cell, &max_ulps_all), None);
+    assert_eq!(empty_cell.debug_ulps_all_tolerance(&rhs, &max_ulps_all), None);
+}
